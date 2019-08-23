76400e2afe22143b61c1bf8d54e33af7bc137972
James Newell <james.newell@automattic.com>
2019-08-23 16:47:22 +1000

    attempt quoting config file

--- .ci/deploy.sh
+++ .ci/deploy.sh
@@ -2,9 +2,9 @@
 source ".ci/utils/strict.sh"
 
 if [ "${CIRCLE_BRANCH}" == "master" ]; then
-    yarn now --token $ZEIT_TOKEN --prod --local-config=../.now.json website/public
-    yarn now --token $ZEIT_TOKEN --prod --local-config=../.now.json playground/storybook-static
+    yarn now --token $ZEIT_TOKEN --prod --local-config="../.now.json" website/public
+    yarn now --token $ZEIT_TOKEN --prod --local-config="../.now.json" playground/storybook-static
 else
-    yarn now --token $ZEIT_TOKEN --local-config=../.now.json website/public
-    yarn now --token $ZEIT_TOKEN --local-config=../.now.json playground/storybook-static
+    yarn now --token $ZEIT_TOKEN --local-config="../.now.json" website/public
+    yarn now --token $ZEIT_TOKEN --local-config="../.now.json" playground/storybook-static
 fi
