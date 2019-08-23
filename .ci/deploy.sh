ff9cd474f989d0839648fef42496f1123eb1bb42
James Newell <james.newell@automattic.com>
2019-08-23 17:55:03 +1000

    cd up

--- .ci/deploy.sh
+++ .ci/deploy.sh
@@ -2,9 +2,9 @@
 source ".ci/utils/strict.sh"
 
 if [ "${CIRCLE_BRANCH}" == "master" ]; then
-    cd website/public && yarn now --token $ZEIT_TOKEN --prod --local-config="../now.json" && cd ..
-    cd playground/storybook-static && yarn now --token $ZEIT_TOKEN --prod --local-config="../now.json" && cd ..
+    cd website/public && yarn now --token $ZEIT_TOKEN --prod --local-config="../now.json" && cd ../..
+    cd playground/storybook-static && yarn now --token $ZEIT_TOKEN --prod --local-config="../now.json" && cd ../..
 else
-    cd website/public && yarn now --token $ZEIT_TOKEN --local-config="../now.json" && cd ..
-    cd playground/storybook-static && yarn now --token $ZEIT_TOKEN --local-config="../now.json" && cd ..
+    cd website/public && yarn now --token $ZEIT_TOKEN --local-config="../now.json" && cd ../..
+    cd playground/storybook-static && yarn now --token $ZEIT_TOKEN --local-config="../now.json" && cd ../..
 fi
