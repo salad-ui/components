1bb297dcdc45e1cd45f9badc3525052cde8ea98b
James Newell <james.newell@automattic.com>
2019-08-23 16:58:47 +1000

    configure npm

--- .ci/version_and_publish.sh
+++ .ci/version_and_publish.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
 source ".ci/utils/strict.sh"
 
-# git remote set-url origin https://github.com/jameslnewell/design-system-example.git
+# configure NPM
+cat < .npmrc
+//registry.npmjs.org/:_authToken=$NPM_AUTH_TOKEN
+EOF
 
+# configure GIT
 git config user.email "ci-server@example.com"
 git config user.name "CI Server"
 
+# version and publish
 git pull
 yarn build-releases version --commit
 git push origin master
