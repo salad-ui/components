99d427aa6bc4cc24b3edcca2ff60b0d1e0a332e5
James Newell <james.newell@automattic.com>
2019-08-23 15:59:05 +1000

    remove comment

--- .ci/deploy.sh
+++ .ci/deploy.sh
@@ -6,7 +6,6 @@ if [ "${CIRCLE_BRANCH}" == "master" ]; then
     now --token $ZEIT_TOKEN website/public --local-config website/.now.json alias
     now --token $ZEIT_TOKEN playground/storybook-static
     now --token $ZEIT_TOKEN playground/storybook-static --local-config playground/.now.json alias
-    ## TODO: alias to an actual domain
 else
     now --token $ZEIT_TOKEN website/public --local-config website/.now.json 
     now --token $ZEIT_TOKEN playground/storybook-static --local-config playground/.now.json 
