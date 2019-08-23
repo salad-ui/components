705aa1fd29e7d72272556b0d46f1dc97de2c7071
James Newell <james.newell@automattic.com>
2019-08-23 16:15:00 +1000

    run now properly

--- .ci/deploy.sh
+++ .ci/deploy.sh
@@ -2,11 +2,11 @@
 source ".ci/utils/strict.sh"
 
 if [ "${CIRCLE_BRANCH}" == "master" ]; then
-    now --token $ZEIT_TOKEN website/public --local-config website/.now.json 
-    now --token $ZEIT_TOKEN website/public --local-config website/.now.json alias
-    now --token $ZEIT_TOKEN playground/storybook-static
-    now --token $ZEIT_TOKEN playground/storybook-static --local-config playground/.now.json alias
+    yarn now --token $ZEIT_TOKEN website/public --local-config website/.now.json 
+    yarn now --token $ZEIT_TOKEN website/public --local-config website/.now.json alias
+    yarn now --token $ZEIT_TOKEN playground/storybook-static
+    yarn now --token $ZEIT_TOKEN playground/storybook-static --local-config playground/.now.json alias
 else
-    now --token $ZEIT_TOKEN website/public --local-config website/.now.json 
-    now --token $ZEIT_TOKEN playground/storybook-static --local-config playground/.now.json 
+    yarn now --token $ZEIT_TOKEN website/public --local-config website/.now.json 
+    yarn now --token $ZEIT_TOKEN playground/storybook-static --local-config playground/.now.json 
 fi
