ef799b70847fb13709d7548eb8f44a7845a2a466
James Newell <james.newell@automattic.com>
2019-08-28 20:26:49 +1000

    use github token

--- .ci/version_and_publish.sh
+++ .ci/version_and_publish.sh
@@ -4,13 +4,13 @@ source ".ci/utils/strict.sh"
 # configure NPM
 yarn lerna exec echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" > ~/.npmrc 
 
-# configure GIT
-git config user.email "ci-server@example.com"
-git config user.name "CI Server"
+# configure GIT - set git user to the commit we are building from
+git config user.name "$(git --no-pager log --format=format:'%an' -n 1)"
+git config user.email "$(git --no-pager log --format=format:'%ae' -n 1)"
 
 # version and publish
 git pull
 yarn build-releases version --commit
-git push origin master
+git push "https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/$GITHUB_REPO.git" master
 yarn build-releases publish --public
-git push --follow-tags
+git push "https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/$GITHUB_REPO.git" --follow-tags
