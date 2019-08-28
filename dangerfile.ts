e8d4323675805f3716de5d0925f943e77d35d0d0
James Newell <james.newell@automattic.com>
2019-08-28 22:13:09 +1000

    try updating dangerfile

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,6 +1,18 @@
 import {fail, danger} from 'danger';
 
 function assertChangsetExistsWhenAnyPackagesHaveBeenTouched(): void {
+  // if the user has run `yarn changeset` then we're all good!
+  const changeset = danger.git.fileMatch('.changeset/**');
+  if (
+    changeset.created ||
+    changeset.edited ||
+    changeset.modified ||
+    changeset.deleted
+  ) {
+    return;
+  }
+
+  // if the user hasn't run `yarn changeset` and has modified a package, then we've got beef
   const packages = danger.git.fileMatch('packages/*/**');
   if (
     packages.created ||
