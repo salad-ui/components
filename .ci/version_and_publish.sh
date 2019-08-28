caf2f7ecad73a357123e4d603c0e7ff615df06b9
James Newell <james.newell@automattic.com>
2019-08-29 06:25:39 +1000

    use if expressions

--- .ci/version_and_publish.sh
+++ .ci/version_and_publish.sh
@@ -1,21 +1,17 @@
 #!/usr/bin/env bash
 source ".ci/utils/strict.sh"
 
-if [ "${GITHUB_REF}" == "refs/heads/master" ]; then
+# configure NPM
+yarn lerna exec echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" > ~/.npmrc 
 
-  # configure NPM
-  yarn lerna exec echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" > ~/.npmrc 
+# configure GIT - set git user to the commit we are building from
+git config user.name "$(git --no-pager log --format=format:'%an' -n 1)"
+git config user.email "$(git --no-pager log --format=format:'%ae' -n 1)"
+git remote set-url origin "https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git"
 
-  # configure GIT - set git user to the commit we are building from
-  git config user.name "$(git --no-pager log --format=format:'%an' -n 1)"
-  git config user.email "$(git --no-pager log --format=format:'%ae' -n 1)"
-  git remote set-url origin "https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git"
-
-  # version and publish
-  git checkout master
-  yarn build-releases version --commit
-  git push
-  yarn build-releases publish --public
-  git push --tags
-
-fi
+# version and publish
+git checkout master
+yarn build-releases version --commit
+git push
+yarn build-releases publish --public
+git push --tags
