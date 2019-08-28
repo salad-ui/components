4785a04853505984206412a30bc99f01ddee19d8
James Newell <james.newell@automattic.com>
2019-08-28 22:19:36 +1000

    log danger

--- dangerfile.ts
+++ dangerfile.ts
@@ -2,6 +2,7 @@ import {fail, danger} from 'danger';
 
 function assertChangsetExistsWhenAnyPackagesHaveBeenTouched(): void {
   // if the user has run `yarn changeset` then we're all good!
+  console.log('danger: ', danger.git);
   const changeset = danger.git.fileMatch('.changeset/**');
   if (
     changeset.created ||
