2e693d869b04c435c6d41233614453e6ec424c64
James Newell <james.newell@automattic.com>
2019-08-28 21:12:25 +1000

    change git setup

--- .ci/version_and_publish.sh
+++ .ci/version_and_publish.sh
@@ -7,14 +7,11 @@ yarn lerna exec echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" > ~/.npmrc
 # configure GIT - set git user to the commit we are building from
 git config user.name "$(git --no-pager log --format=format:'%an' -n 1)"
 git config user.email "$(git --no-pager log --format=format:'%ae' -n 1)"
-
-# version and publish
-
-echo "https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/$GITHUB_REPO.git"
 git remote set-url origin "https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/$GITHUB_REPO.git"
 
-git pull
+# version and publish
+git pull origin master
 yarn build-releases version --commit
-git push "https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/$GITHUB_REPO.git" master
+git push origin master
 yarn build-releases publish --public
-git push "https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/$GITHUB_REPO.git" --follow-tags
+git push origin --follow-tags
