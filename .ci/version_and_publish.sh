791d6d5da7e7d6f6d224cf6f04b55c3817d8827d
James Newell <james.newell@automattic.com>
2019-09-06 08:56:42 +1000

    attempt to fix publish script

--- .ci/version_and_publish.sh
+++ .ci/version_and_publish.sh
@@ -4,8 +4,7 @@ source ".ci/utils/strict.sh"
 # configure GIT - set git user to the commit we are building from
 git config user.name "$(git --no-pager log --format=format:'%an' -n 1)"
 git config user.email "$(git --no-pager log --format=format:'%ae' -n 1)"
-# git remote set-url origin "https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git"
-git remote set-url origin "https://$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git"
+git remote set-url origin "https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git"
 
 # configure NPM
 yarn lerna exec echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" > ~/.npmrc 
