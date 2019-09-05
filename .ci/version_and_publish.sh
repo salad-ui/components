db91eb62dd39409f733ef3c84d69ea0d73eae3f2
James Newell <james.newell@automattic.com>
2019-09-06 08:37:36 +1000

    attempt to fix publish script

--- .ci/version_and_publish.sh
+++ .ci/version_and_publish.sh
@@ -10,8 +10,7 @@ git config user.email "$(git --no-pager log --format=format:'%ae' -n 1)"
 git remote set-url origin "https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git"
 
 # version and publish
-git checkout master
 yarn changeset bump --commit
 git push
 yarn changeset release --public
-git push --tags
+git push --follow-tags
