4118b247b93d8470933d5b1f24ae40a534a0d6d1
James Newell <james.newell@automattic.com>
2019-09-06 10:49:39 +1000

    attempt to fix publish script

--- .ci/version_and_publish.sh
+++ .ci/version_and_publish.sh
@@ -1,16 +1,15 @@
 #!/usr/bin/env bash
 source ".ci/utils/strict.sh"
 
-# configure GIT - deploy key
+# configure GIT - setup the Github Deploy Key
 mkdir ~/.ssh
 chmod 700 ~/.ssh
 echo "$GITHUB_DEPLOY_KEY" > ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
-echo "github.com,13.237.44.5 ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==" > ~/.ssh/known_hosts
+ssh-keyscan -t rsa github.com > ~/.ssh/known_hosts
 chmod 744 ~/.ssh/known_hosts
-ssh -Tv git@github.com
 
-# configure GIT - set git user to the commit we are building from
+# configure GIT - setup the git user who will make the commit
 git config user.name "$(git --no-pager log --format=format:'%an' -n 1)"
 git config user.email "$(git --no-pager log --format=format:'%ae' -n 1)"
 
