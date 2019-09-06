17de8455dfe55697e58197013f380464716d6156
James Newell <james.newell@automattic.com>
2019-09-06 10:26:20 +1000

    attempt to fix publish script

--- .ci/version_and_publish.sh
+++ .ci/version_and_publish.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
 source ".ci/utils/strict.sh"
 
+# configure GIT - deploy key
+echo "$GITHUB_DEPLOY_KEY" > ~/.ssh/id_rsa
+ssh -T git@github.com
+
 # configure GIT - set git user to the commit we are building from
 git config user.name "$(git --no-pager log --format=format:'%an' -n 1)"
 git config user.email "$(git --no-pager log --format=format:'%ae' -n 1)"
 
 # using a machine user who is an admin of this repo so we can get around the branch restrictions
-echo "https://jameslnewell-bot:$GITHUB_BOT_TOKEN@github.com/$GITHUB_REPOSITORY.git"
-git remote set-url origin "https://jameslnewell-bot:$GITHUB_BOT_TOKEN@github.com/$GITHUB_REPOSITORY.git"
+# echo "https://jameslnewell-bot:$GITHUB_BOT_TOKEN@github.com/$GITHUB_REPOSITORY.git"
+# git remote set-url origin "https://jameslnewell-bot:$GITHUB_BOT_TOKEN@github.com/$GITHUB_REPOSITORY.git"
 
 # configure NPM
 yarn lerna exec echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" > ~/.npmrc 
