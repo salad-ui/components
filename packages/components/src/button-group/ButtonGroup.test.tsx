25f5f9952003b9bd20fee31aa55fa896397b2289
James Newell <james.newell@automattic.com>
2019-09-13 11:41:55 -0400

    fix tests

--- packages/components/src/button-group/ButtonGroup.test.tsx
+++ packages/components/src/button-group/ButtonGroup.test.tsx
@@ -6,22 +6,30 @@ const href = 'http://wordpress.com/';
 
 describe('ButtonGroup', () => {
   test('it renders buttons', () => {
-    const {getByText} = render(
+    const {getByTestId} = render(
       <ButtonGroup size="sm">
-        <ButtonGroup.Button variant="primary" href={href} isDisabled={false}>
+        <ButtonGroup.Button
+          variant="primary"
+          href={href}
+          isDisabled={false}
+          data-testid="button-1"
+        >
           Click me!
         </ButtonGroup.Button>
         <ButtonGroup.Button
           variant="secondary"
           onClick={jest.fn()}
           isDisabled={false}
+          data-testid="button-2"
         >
           Click me!
         </ButtonGroup.Button>
       </ButtonGroup>,
     );
-    const element = getByText('Click me!');
-    expect(element.tagName).toEqual('A');
+    const button1 = getByTestId('button-1');
+    const button2 = getByTestId('button-2');
+    expect(button1.classList.contains('wp-button-group__button')).toBeTruthy();
+    expect(button2.classList.contains('wp-button-group__button')).toBeTruthy();
   });
 
   // TODO: finish test cases
