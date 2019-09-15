0ce04be8c80f691b9818ff851f72b9b7214ed111
James Newell <james.newell@automattic.com>
2019-09-15 09:14:58 -0400

    seperate CSS build

--- packages/components/src/button-group/index.ts
+++ packages/components/src/button-group/index.ts
@@ -1,2 +1,1 @@
 export * from './ButtonGroup';
-import './ButtonGroup.scss';
--- website/src/components/AppProvider/index.tsx
+++ website/src/components/AppProvider/index.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import '@design-system-example/components/index.scss';
 
 const AppProvider: React.FC = props => {
   const {children} = props;
