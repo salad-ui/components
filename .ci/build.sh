b5f1e8688454ef10b93881d48eac4be7759233dc
James Newell <james.newell@automattic.com>
2019-10-02 08:52:41 +1000

    increase watcher limit

--- .ci/build.sh
+++ .ci/build.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 source ".ci/utils/strict.sh"
 
+# increase watchers for gatsby
+# @see https://github.com/gatsbyjs/gatsby/issues/11406#issuecomment-458769756
+echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
+
 yarn run build
