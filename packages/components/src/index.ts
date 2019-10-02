284c1b46fcef31fa02f1707bb04ff40f1addfccc
James Newell <james.newell@automattic.com>
2019-10-02 13:52:02 +1000

    :allthethings: package

---
+++ packages/components/src/index.ts
@@ -0,0 +1,6 @@
+export * from '@salad-ui/button';
+export * from '@salad-ui/card';
+export * from '@salad-ui/field';
+export * from '@salad-ui/text-input';
+export * from '@salad-ui/theme';
+export * from '@salad-ui/typography';
