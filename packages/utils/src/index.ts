21b7e10b15e82249e7bc3e2d0cf9f94e92923cf5
James Newell <james.newell@automattic.com>
2019-10-04 17:06:55 +1000

    explicitly add dependency

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,7 +1,9 @@
 import {css} from 'styled-components';
 import {getColor} from '@salad-ui/color';
 
-export const focusStyle = (colorName?: string) => css`
-  outline: none;
-  box-shadow: inset 0 0 0 2px ${colorName ? getColor(colorName) : '#2271B1'};
-`;
+export const focusStyle = (colorName?: string) => {
+  return css`
+    outline: none;
+    box-shadow: inset 0 0 0 2px ${colorName ? getColor(colorName) : '#2271B1'};
+  `;
+};
